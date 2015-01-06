938db96e6204b4e009004e4df043cc567d31daf9
Akshat Khariwal <akshat.khariwal@hotmail.com>
2015-01-06 14:14:13 +0000

    cleaning

--- AKMain.hs
+++ AKMain.hs
@@ -1,19 +1,11 @@
 import AKDownload
 import AKDatabase
 
---insertInDB (movieData:[]) = insertQuery movieData
---insertInDB (movieData:movieDataList) = insertQuery movieData : insertInDB movieDataList
-
-vari = do
+insertInDB = do
 	d <- boxOffice
 	let a = movies d
-	mapM decodeBoxOffice a
+	mapM callInsertQuery a
 	--return a
 
---insertQuery = decodeBoxOffice vari
-
---decodeBoxOffice (bO:[]) = print $ AKDownload.id bO
---decodeBoxOffice (bO:bOs) = print $ AKDownload.id bO : decodeBoxOffice bOs
-
-decodeBoxOffice bO = addData bO
+callInsertQuery bO = addData bO
 
