515903224e3e7f4f866af796fc1de26a1d6a368d
Akshat Khariwal <akshat.khariwal@hotmail.com>
2015-01-06 17:12:04 +0000

    check before insert

--- AKDatabase.hs
+++ AKDatabase.hs
@@ -24,24 +24,25 @@ prepDB dbh =
 
 --addData :: IConnection conn => conn -> Podcast -> IO Podcast
 addData bO= 
-    handleSql errorHandler $
-      do -- Insert the castURL into the table.  The database
+	handleSql errorHandler $
+		do -- Insert the castURL into the table.  The database
          -- will automatically assign a cast ID.
 --	 let dbh = conn
-	 d <- connectSqlite3 "test1.db"
-	 prepDB d
-         run d "INSERT INTO movies (id, title) VALUES (?,?)" [toSql (AKDownload.id bO), toSql (AKDownload.title bO)]
-	 commit d
-	 disconnect d
+			d <- connectSqlite3 "test1.db"
+			prepDB d
+			check <- getData d $ AKDownload.id bO
+			case check of
+				[[SqlNull]] -> (run d "INSERT INTO movies (id, title) VALUES (?,?)"
+					[toSql (AKDownload.id bO), toSql (AKDownload.title bO)])
+				[x] -> return 0
+			commit d
+			disconnect d
          -- Find out the castID for the URL we just added.
-    where errorHandler e = 
-              do fail $ "Error adding podcast; does this URL already exist?\n"
-                     ++ show e
+		where errorHandler e = 
+			do fail $ "Error adding podcast; does this URL already exist?\n" ++ show e
 
 
---getData dbh = 
---	do 
---	run' dbh "SELECT title FROM movies"
-		--WHERE id = ?" [toSql ("123")]
-	--show r
---	return r
+getData dbh movieId = 
+	do 
+	r <- quickQuery' dbh "SELECT * from movies where id = ?" [toSql (movieId)]
+	return r
