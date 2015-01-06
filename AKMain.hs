ebbc3fdf9a93250d88d56c08e1a0d08f150a80dd
Akshat Khariwal <akshat.khariwal@hotmail.com>
2015-01-06 11:39:45 +0000

    commit

--- AKMain.hs
+++ AKMain.hs
@@ -4,4 +4,4 @@ import AKDatabase
 --insertInDB (movieData:[]) = insertQuery movieData
 --insertInDB (movieData:movieDataList) = insertQuery movieData : insertInDB movieDataList
 
---insertQuery = map print boxOffice
+insertQuery = map putStr boxOffice
