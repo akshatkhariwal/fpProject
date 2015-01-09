cec5a2a73d605ca51952fec7114b65fca9e1729f
Akshat Khariwal <akshat.khariwal@hotmail.com>
2015-01-09 10:40:39 +0000

    Database Insertion completed

--- AKDatabase.hs
+++ AKDatabase.hs
@@ -47,17 +47,43 @@ addData bO=
 				[] -> (run d "INSERT INTO movies (id, title, year, synopsis) VALUES (?,?,?,?)"
 					[toSql (AKDownload.id bO), toSql (AKDownload.title bO),
 						toSql (AKDownload.year bO), toSql (AKDownload.synopsis bO)])
-				[x] -> return 0
+				[x] -> (run d "UPDATE movies SET title = ?, year = ?, synopsis = ? WHERE id = ?" 
+            				[toSql (AKDownload.synopsis bO), toSql (AKDownload.title bO),
+						toSql (AKDownload.year bO), toSql (AKDownload.id bO)])
+			mapM (addCastData d (AKDownload.id bO)) $ AKDownload.abridged_cast bO
 			commit d
 			disconnect d
-			mapM (addCastData (AKDownload.id bO)) $ AKDownload.abridged_cast bO
          -- Find out the castID for the URL we just added.
 		where errorHandler e = 
 			do fail $ "Error adding podcast; does this URL already exist?\n" ++ show e
 
-addCastData movie_id cast = print ((AKDownload.name cast) ++ movie_id)
+addCastData dbh movie_id cast =
+	do
+	check <- getCastData dbh $ AKDownload.cast_id cast
+	case check of
+		[] -> (run dbh "INSERT INTO cast (id, name) VALUES (?,?)"
+			[toSql (AKDownload.cast_id cast), toSql (AKDownload.name cast)])
+		[x] -> (run dbh "UPDATE cast SET name = ? WHERE id = ?" 
+            		[toSql (AKDownload.name cast), toSql (AKDownload.cast_id cast)])
+	check <- getCastBridgeData dbh movie_id $ AKDownload.cast_id cast
+	case check of
+		[] -> (run dbh "INSERT INTO cast_bridge (movie_id, cast_id) VALUES (?,?)"
+			[toSql movie_id, toSql (AKDownload.cast_id cast)])
+		[x] -> return 0
 
 getMovieData dbh movieId = 
 	do 
-	r <- quickQuery' dbh "SELECT * from movies where id = ?" [toSql (movieId)]
+	r <- quickQuery' dbh "SELECT * from movies WHERE id = ?" [toSql movieId]
 	return r
+
+getCastData dbh cast_id = 
+	do 
+	r <- quickQuery' dbh "SELECT * from cast WHERE id = ?" [toSql cast_id]
+	return r
+
+getCastBridgeData dbh movie_id cast_id = 
+	do 
+	r <- quickQuery' dbh "SELECT * from cast_bridge WHERE movie_id = ? AND cast_id = ?" [toSql movie_id, toSql cast_id]
+	return r
+
+
